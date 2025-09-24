3230e6ebb25328f51c11383345bee3c1c0b9fe06
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-09-24 10:31:51 +0000

    fb

--- resources/views/dashboard/layout-dashboard/navbar.blade.php
+++ resources/views/dashboard/layout-dashboard/navbar.blade.php
@@ -86,9 +86,17 @@
                          <div class="dropdown-divider my-1"></div>
                      </li>
                      <li>
-                         <a class="dropdown-item" href="javascript:void(0);">
+                        <form method="POST" action="{{ route('logout') }}" class="d-flex align-items-center">
+                            @csrf
+                            <button type="submit" class="dropdown-item">
+                                <i class="icon-base bx bx-power-off icon-md me-3"></i><span>Log Out</span>
+                            </button>
+                        </form>
+
+                         {{-- <a class="dropdown-item" href="javascript:void(0);">
                              <i class="icon-base bx bx-power-off icon-md me-3"></i><span>Log Out</span>
-                         </a>
+                         </a> --}}
+
                      </li>
                  </ul>
              </li>
