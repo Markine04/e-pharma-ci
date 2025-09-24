fc40076735f646c1bcc25df98928f75dcf9f9690
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-09-24 09:32:09 +0000

    ji

--- resources/views/dashboard/layout-dashboard/sidebar.blade.php
+++ resources/views/dashboard/layout-dashboard/sidebar.blade.php
@@ -69,56 +69,15 @@
 
   <ul class="menu-inner py-1">
     <!-- Dashboards -->
-    <li class="menu-item {{(Route::is('dashboard.*')?'active open':'')}}">
-      <a href="javascript:void(0);" class="menu-link menu-toggle">
+
+    <li class="menu-item  {{(Route::is('dashboard')?'active open':'')}}">
+      <a href="{{route('dashboard')}}" class="menu-link">
         <i class="menu-icon tf-icons bx bx-home-smile"></i>
         <div class="text-truncate" data-i18n="Dashboards">Dashboards</div>
-        {{-- <span class="badge rounded-pill bg-danger ms-auto">5</span> --}}
       </a>
-      {{-- <ul class="menu-sub">
-        <li class="menu-item active">
-          <a href="index.html" class="menu-link">
-            <div class="text-truncate" data-i18n="Analytics">Analytics</div>
-          </a>
-        </li>
-        <li class="menu-item">
-          <a
-            href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/html/vertical-menu-template/dashboards-crm.html"
-            target="_blank"
-            class="menu-link">
-            <div class="text-truncate" data-i18n="CRM">CRM</div>
-            <div class="badge rounded-pill bg-label-primary text-uppercase fs-tiny ms-auto">Pro</div>
-          </a>
-        </li>
-        <li class="menu-item">
-          <a
-            href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/html/vertical-menu-template/app-ecommerce-dashboard.html"
-            target="_blank"
-            class="menu-link">
-            <div class="text-truncate" data-i18n="eCommerce">eCommerce</div>
-            <div class="badge rounded-pill bg-label-primary text-uppercase fs-tiny ms-auto">Pro</div>
-          </a>
-        </li>
-        <li class="menu-item">
-          <a
-            href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/html/vertical-menu-template/app-logistics-dashboard.html"
-            target="_blank"
-            class="menu-link">
-            <div class="text-truncate" data-i18n="Logistics">Logistics</div>
-            <div class="badge rounded-pill bg-label-primary text-uppercase fs-tiny ms-auto">Pro</div>
-          </a>
-        </li>
-        <li class="menu-item">
-          <a
-            href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/html/vertical-menu-template/app-academy-dashboard.html"
-            target="_blank"
-            class="menu-link">
-            <div class="text-truncate" data-i18n="Academy">Academy</div>
-            <div class="badge rounded-pill bg-label-primary text-uppercase fs-tiny ms-auto">Pro</div>
-          </a>
-        </li>
-      </ul> --}}
     </li>
+
+
 
     <!-- Layouts -->
     <li class="menu-item {{(Route::is('medicaments.*')?'active open':'')}} {{(Route::is('ordonnances.*')?'active open':'')}}">
@@ -136,6 +95,7 @@
         <li class="menu-item {{(Route::is('ordonnances.index')?'active':'')}}">
           <a href="{{route('ordonnances.index')}}" class="menu-link">
             <div class="text-truncate" data-i18n="Without navbar">Ordonnances</div>
+            <span class="badge rounded-pill bg-danger ms-auto">5</span>
           </a>
         </li>
         <li class="menu-item">
