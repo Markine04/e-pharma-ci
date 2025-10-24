670db45e27bf32dfd2ab8e5a27137a79c9a75e3e
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-10-24 20:09:35 +0000

    fgb

--- routes/api.php
+++ routes/api.php
@@ -104,8 +104,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/panier-show', [CartsController::class, 'show']);
 
-    Route::get('/paniers/{id}', [CartsController::class, 'get_panier']);
-    // Route::get('/paniers/{id}', [CartsController::class, 'show']);
+    // Route::get('/paniers/{id}', [CartsController::class, 'get_panier']);
+    Route::get('/paniers/{id}', [CartsController::class, 'show']);
 
     Route::get('/panier-delete/{id}', [CartsController::class, 'delete_from_cart']);
 
