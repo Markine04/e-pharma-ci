f692012ee5355cbe1aba028f37f3b2b340bbaf6a
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-11-06 16:13:54 +0000

    Update CartsController.php

--- app/Http/Controllers/Api/CartsController.php
+++ app/Http/Controllers/Api/CartsController.php
@@ -161,7 +161,7 @@ class CartsController extends Controller
             ->where('commandes.idcommande', $idcommande)
             ->where('commandes.numerocommande', $numerocommande)
             ->select('commandes.statut')
-            ->first(); // ✅ retourne un seul résultat
+            ->get(); // ✅ retourne un seul résultat
 
         return response()->json([
             'suivicommandes' => $suivicommandes,
