6f2f228e96ccdf7f7fbd2b5d88ae877c2a6e6687
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-11-08 03:41:22 +0000

    Update UploadController.php

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -74,7 +74,7 @@ class UploadController extends Controller
     public function show(string $id, Request $request)
     {
         $detailsordonnances = DB::table('ordonnance_clients')
-            ->join('clients', 'ordonnance_clients.id_client', '=', 'clients.id')
+            ->join('users', 'ordonnance_clients.id_client', '=', 'users.id')
             ->join('pharmacies', 'ordonnance_clients.id_pharmacie', '=', 'pharmacies.idpharmacie')
             ->select('ordonnance_clients.*', 'pharmacies.*')
             ->where('id_client', $request->user()->id)
