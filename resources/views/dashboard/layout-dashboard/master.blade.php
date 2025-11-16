2ef4095fbea392cc3200fcbb54da9404a01d7daf
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-11-16 17:32:14 +0000

    Update master.blade.php

--- resources/views/dashboard/layout-dashboard/master.blade.php
+++ resources/views/dashboard/layout-dashboard/master.blade.php
@@ -107,12 +107,12 @@
 
     {{-- <script src="{{ asset('dashboards/assets/vendor/libs/jquery/jquery.js') }}"></script> --}}
 
-    {{-- <script src="{{ asset('dashboards/assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('dashboards/assets/vendor/libs/popper/popper.js') }}"></script>
     <script src="{{ asset('dashboards/assets/vendor/js/bootstrap.js') }}"></script>
 
     <script src="{{ asset('dashboards/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
 
-    <script src="{{ asset('dashboards/assets/vendor/js/menu.js') }}"></script> --}}
+    <script src="{{ asset('dashboards/assets/vendor/js/menu.js') }}"></script>
 
     <!-- endbuild -->
 
