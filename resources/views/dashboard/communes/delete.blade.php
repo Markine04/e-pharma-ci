e165427b8346483e1f28e048fabe7dcc81840b71
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-11-24 19:06:17 +0000

    Update delete.blade.php

--- resources/views/dashboard/communes/delete.blade.php
+++ resources/views/dashboard/communes/delete.blade.php
@@ -1,12 +1,11 @@
 <form action="{{ route('communes.destroy') }}" method="post">
     @csrf
-    <input type="hidden" name="commune" value="{{ $commune->idcommune }}">
     <div class="text-center">
         <h4>Voulez-vous vraiment supprimer cette commune?</h4>
         <hr>
         <input type="hidden" name="id" value="{{ $commune->idcommune }}">
 
-        <a href="{{route('communes.index')}}" class="btn bg-gradient-danger text-white">Non</a>
+        <a href="{{route('communes.index')}}" class="btn btn-danger">Non</a>
 
         <button type="submit" class="btn btn-primary">Oui</button>
     </div>
