ad4f78b877373b90d4f115cd762807228ec33764
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-11-24 09:22:32 +0000

    VDF

--- resources/views/dashboard/pharmacies/create.blade.php
+++ resources/views/dashboard/pharmacies/create.blade.php
@@ -1,8 +1,5 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<link rel="stylesheet" href="{{ asset('assets/builds/css/select2.min.css') }}" />
-<script src="{{ asset('assets/builds/js/select2.min.js') }}"></script>
-
 
 <form method="post" id="formSchedule" action="{{ route('pharmacies.store') }}" enctype="multipart/form-data">
     @csrf
