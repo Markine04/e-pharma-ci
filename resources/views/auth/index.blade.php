5b92d6ae9ced0b7ee2d10bedd4e5c550d97e897a
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-12-01 19:26:48 +0000

    Update index.blade.php

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -83,7 +83,7 @@
                                                             data-url="{{ route('assurance.cartAssurance', ['id' => $items->id]) }}"
                                                             class="dropdown-item" data-bs-toggle="modal"
                                                             data-ajax-popup="true" data-size="lg"
-                                                            data-title="voir la carte assurance" style="cursor: pointer"><i
+                                                            data-title="voir les cartes assurance" style="cursor: pointer"><i
                                                                 class="icon-eyes"></i>
                                                         </a>
                                                     </li>
