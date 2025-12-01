7fb26a53cc9e423efe765788184ff44134457600
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-12-01 19:55:09 +0000

    Update voir-image.blade.php

--- resources/views/dashboard/assurances/voir-image.blade.php
+++ resources/views/dashboard/assurances/voir-image.blade.php
@@ -2,35 +2,39 @@
 <html lang="en">
 
 <style>
-            /* Style pour l'image miniature */
-            .thumbnail {
-                width: 200px;
-                cursor: pointer;
-                transition: transform 0.2s;
-            }
+    /* Style pour l'image miniature */
+    .thumbnail {
+        width: 200px;
+        cursor: pointer;
+        transition: transform 0.2s;
+    }
 
-            .thumbnail:hover {
-                transform: scale(1.05);
-            }
+    .thumbnail:hover {
+        transform: scale(1.05);
+    }
 
-            /* Style pour la version agrandie */
-            .fullImage {
-                display: none;
-                position: fixed;
-                top: 0;
-                left: 0;
-                width: 50%;
-                height: 50%;
-                object-fit: contain;
-                background: rgba(0, 0, 0, 0.8);
-                cursor: zoom-out;
-            }
-        </style>
+    /* Style pour la version agrandie */
+    .fullImage {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 50%;
+        height: 50%;
+        object-fit: contain;
+        background: rgba(0, 0, 0, 0.8);
+        cursor: zoom-out;
+    }
+</style>
 
 <body style="background-color:rgba(0, 0, 0, 0.8);">
-    <center>
-        <img src="{{ asset('storage/assurances-cartes/' . $assurances->images) }}"
-                                            alt="{{ $assurances->id_assurance }}">
-    </center>
+    <div class="fullImage">
+        <center>
+            <img src="{{ asset('storage/assurances-cartes/' . $assurances->images) }}"
+                alt="{{ $assurances->id_assurance }}">
+        </center>
+    </div>
+
 </body>
+
 </html>
