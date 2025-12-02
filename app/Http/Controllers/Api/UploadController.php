dc493120d8f082e7f565909f90d32fb28b160df1
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-12-02 11:46:32 +0000

    Update UploadController.php

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -44,7 +44,7 @@ class UploadController extends Controller
         if ($request->hasFile('image')) {
             $file = $request->file('image');
             $name = time() . '.' . $file->getClientOriginalExtension();
-            $file->storeAs('ordonnances-clients/', $name, 'public');
+            // $file->storeAs('ordonnances-clients/', $name, 'public');
             $file->move(public_path('storage/ordonnances-clients/'), $name);
 
             DB::table('ordonnance_clients')->insert([
